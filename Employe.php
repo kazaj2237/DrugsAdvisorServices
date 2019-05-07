ed3199871d17a066cf0fb44d881161285055f8d3
kazaj2237 <zaleiij@gmail.com>
2019-05-07 18:38:53 +0200

    classe Employe  && Role

---
+++ Employe.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Junior ZALE
+ * Date: 07/05/2019
+ * Time: 18:26
+ */
+
+class Employe
+{
+  private $idEmploye;
+  private $dateDebut;
+  private $dateFin;
+
+    /**
+     * @return mixed
+     */
+    public function getIdEmploye()
+    {
+        return $this->idEmploye;
+    }
+
+    /**
+     * @param mixed $idEmploye
+     */
+    public function setIdEmploye($idEmploye)
+    {
+        $this->idEmploye = $idEmploye;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+
+    /**
+     * @param mixed $dateDebut
+     */
+    public function setDateDebut($dateDebut)
+    {
+        $this->dateDebut = $dateDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateFin()
+    {
+        return $this->dateFin;
+    }
+
+    /**
+     * @param mixed $dateFin
+     */
+    public function setDateFin($dateFin)
+    {
+        $this->dateFin = $dateFin;
+    }
+
+
+}
