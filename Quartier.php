41d08fe32d339ce435f092ff3b9e7187874132d6
ruthel <balockruthel.cedrickascens@gmail.com>
2019-05-07 17:40:19 +0100

    creaion de classe quartier et horaire

---
+++ Quartier.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RUTHEL PC
+ * Date: 07/05/2019
+ * Time: 17:35
+ */
+
+class Quartier
+{
+    private $idQuartier;
+    private $nom;
+
+    /**
+     * @return mixed
+     */
+    public function getIdQuartier()
+    {
+        return $this->idQuartier;
+    }
+
+    /**
+     * @param mixed $idQuartier
+     */
+    public function setIdQuartier($idQuartier)
+    {
+        $this->idQuartier = $idQuartier;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+}
