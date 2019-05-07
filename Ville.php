e03bdb12bf2c1733c4fd9fc5b28d14f9e8d9d810
Curtis NJIKI DJAPA P <njikicurtis@gmail.com>
2019-05-07 17:59:27 +0100

    Pharmacie et Ville

---
+++ Ville.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Curtis NJIKI DJAPA P
+ * Date: 07/05/2019
+ * Time: 17:56
+ */
+
+class Ville
+{
+    private $idVille;
+    private $nom;
+
+    /**
+     * @return mixed
+     */
+    public function getIdVille()
+    {
+        return $this->idVille;
+    }
+
+    /**
+     * @param mixed $idVille
+     */
+    public function setIdVille($idVille)
+    {
+        $this->idVille = $idVille;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+
+}
